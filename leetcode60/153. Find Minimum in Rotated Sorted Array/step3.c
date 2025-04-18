372c9cc7c2c4782bf67200d1eb801c131b7c3338
takuyasakamoto <takuya50719@g.ecc.u-tokyo.ac.jp>
2025-04-18 13:30:47 +0900

    153. Find Minimum in Rotated Sorted Array

--- leetcode60/153. Find Minimum in Rotated Sorted Array/step3.c
+++ leetcode60/153. Find Minimum in Rotated Sorted Array/step3.c
@@ -14,11 +14,11 @@ int findMin(int* nums, int numsSize) {
 }
 
 int findMin(int* nums, int numsSize) {
-    int left = 0, right = numsSize - 1, target = nums[0];
-    while (left <= right) {
+    int left = 0, right = numsSize, target = nums[0];
+    while (left < right) {
         int mid = left + (right - left) / 2;
         if (nums[mid] < target) {
-            right = mid - 1;
+            right = mid;
             target = nums[mid];
         }
         else {
